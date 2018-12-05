2886d7f48160a3a08393c0e33704798f24ed2671
Monty Anderson <work@montyanderson.net>
2018-12-05 18:19:53 +0000

    queues

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const Router = require('koa-router');
 const serve = require('koa-static');
 const axios = require('axios');
 const { spawn } = require('child_process');
+const Speaker = require('speaker');
 
 const Tidal = require('./lib/Tidal');
 
@@ -15,32 +16,68 @@ router.get('/api/search', async ctx => {
 	ctx.body = await tidal.search(ctx.query.query);
 });
 
-let streamer, player;
+let player, streamer;
+
+const queue = [];
+
+function stop() {
+	if(player) {
+		player.kill('SIGSTOP');
+		streamer.destroy();
+	}
+}
+
+async function play(url) {
+	stop();
+
+	player = spawn('play', [ '-', '--bits', '16', '--rate', '44100' ]);
+
+	player.on('exit', async () => {
+		await next();
+	});
+
+	await new Promise(resolve => setTimeout(resolve, 500));
+
+	streamer = (await axios.get(url, {
+		responseType: 'stream'
+	})).data;
+
+	streamer.pipe(player.stdin);
+}
+
+async function next() {
+	stop();
+
+	if(queue.length > 0)
+		await play(queue.shift());
+}
 
 router.get('/api/play', async ctx => {
 	const url = await tidal.getTrackUrl(ctx.query.id);
 
-	if(player) {
-		streamer.destroy();
-		player.kill('SIGSTOP');
+	await play(url);
+
+	ctx.body = JSON.stringify(queue);
+});
+
+router.get('/api/queue', async ctx => {
+	const url = await tidal.getTrackUrl(ctx.query.id);
+
+	console.log(player);
+
+	if(!player) {
+		play(url);
+	} else {
+		queue.push(url);
 	}
 
-	player = spawn('play', [
-		'-'
-	]);
+	ctx.body = JSON.stringify(queue);
+});
 
-	await new Promise(resolve => setTimeout(resolve, 1000));
+router.get('/api/next', async ctx => {
+	await next();
 
-	const { data } = await axios.get(url, {
-		responseType: 'stream'
-	});
-
-	streamer = data;
-
-	streamer.pipe(player.stdin);
-
-	player.stdout.on('data', data => console.log('stdout', data.toString()));
-	player.stderr.on('data', data => console.log('stderr', data.toString()));
+	ctx.body = JSON.stringify(queue);
 });
 
 app
--- src/components/App.vue
+++ src/components/App.vue
@@ -4,9 +4,11 @@
 
 		<input v-model="query" v-on:keyup="search" type="text" class="form-control" placeholder="Search">
 
+		<h1 v-on:click="next">Next</h1>
+
 		<div class="row container">
 			<div v-for="result in results" class="col-12">
-				<h4 v-on:click="play(result)">{{result.title}}</h4>
+				<h4><span v-on:click="play(result)">{{result.title}}</span> <a v-on:click="pushToQueue(result)">Queue</a></h4>
 				<h5>
 					<span v-for="artist in result.artists" class="label label-default">
 						{{artist.name}}
@@ -23,7 +25,8 @@ const axios = require('axios');
 module.exports = {
 	data: () => ({
 		query: '',
-		results: []
+		results: [],
+		queue: []
 	}),
 	methods: {
 		async search() {
@@ -37,11 +40,29 @@ module.exports = {
 		},
 
 		async play({ id }) {
-			await axios.get('/api/play', {
+			const { data } = await axios.get('/api/play', {
 				params: {
 					id
 				}
 			});
+
+			this.queue = data;
+		},
+
+		async pushToQueue({ id }) {
+			const { data } = await axios.get('/api/queue', {
+				params: {
+					id
+				}
+			});
+
+			this.queue = data;
+		},
+
+		async next() {
+			const { data } = await axios.get('/api/next');
+
+			this.queue = data;
 		}
 	}
 };
